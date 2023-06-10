d5e9dec51fd825e7867aa703987022ab5a990727
desaijay26 <desai26jay@gmail.com>
2023-06-10 15:44:05 +0530

    app config data for common configurations

--- app-config-data/src/main/java/com/djaytech/appconfigdata/AppConfigDataApplication.java
+++ app-config-data/src/main/java/com/djaytech/appconfigdata/AppConfigDataApplication.java
@@ -1,4 +1,4 @@
-package com.example.appconfigdata;
+package com.djaytech.appconfigdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app-config-data/src/test/java/com/djaytech/appconfigdata/AppConfigDataApplicationTests.java
+++ app-config-data/src/test/java/com/djaytech/appconfigdata/AppConfigDataApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.appconfigdata;
+package com.djaytech.appconfigdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
