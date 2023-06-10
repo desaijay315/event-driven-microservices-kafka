dc438e957f4e3e0dd04190ef1667441be7e08cff
desaijay26 <desai26jay@gmail.com>
2023-06-10 15:25:00 +0530

    app config data configuration

--- twitter-to-kafka-service/src/main/java/com/djaytech/twittertokafkaservice/runner/impl/TwitterKafkaStreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/djaytech/twittertokafkaservice/runner/impl/TwitterKafkaStreamRunner.java
@@ -5,6 +5,7 @@ import com.djaytech.twittertokafkaservice.listener.TwitterKafkaStatusListener;
 import com.djaytech.twittertokafkaservice.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import twitter4j.FilterQuery;
 import twitter4j.TwitterException;
@@ -15,6 +16,7 @@ import javax.annotation.PreDestroy;
 import java.util.Arrays;
 
 @Component
+@ConditionalOnProperty(name = "twitter-to-kafka-service.enable-mock-tweets", havingValue = "false")
 public class TwitterKafkaStreamRunner implements StreamRunner {
 
     private static final Logger LOG =  LoggerFactory.getLogger(TwitterKafkaStatusListener.class);
